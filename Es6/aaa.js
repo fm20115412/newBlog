cc4a009c2c53b219aeac0f647ab88b69d00aff0c
641290900@qq.com <641290900@qq.com>
2019-09-13 21:06:35 +0800

    add this关键字

--- Es6/aaa.js
+++ Es6/aaa.js
@@ -44,4 +44,58 @@ if(a){
         console.log('b')
     }
 } */
+function foo() {
+    console.log(this.a);
+}
+var obj = {
+    a: 2,
+    foo: foo
+};
+var bar = obj.foo; // 函数别名!
+var a = "oops, global"; // a 是全局对象的属性 bar(); // "oops, global"
 
+function foo() {
+    console.log(this.a);
+}
+var obj = {
+    a: 2,
+    foo: foo
+};
+var a = "oops, global"; // a 是全局对象的属性 
+setTimeout(obj.foo, 100); // "oops, global"
+
+function setTimeout(fn, delay) { // 等待 delay 毫秒
+    fn(); // <-- 调用位置!
+}
+
+function foo() {
+    console.log(this.a);
+}
+var obj = {
+    a: 2
+};
+foo.call(obj); // 2
+
+function greet(l1, l2, l3) {
+    alert(
+        `Hello, my name is ${this.name} and I know ${l1}, ${l2}, and ${l3}`
+    )
+}
+const user = {
+    name: 'Tyler',
+    age: 27,
+}
+const languages = ['JavaScript', 'Ruby', 'Python']
+greet.call(user, languages[0], languages[1], languages[2])
+
+
+function foo() {
+    console.log(this.a);
+}
+var a = 2;
+foo.call(null); // 2
+
+function foo(a) {
+    this.a = a;
+}
+var bar = new foo(2); console.log(bar.a); // 2
